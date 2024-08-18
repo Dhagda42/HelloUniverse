433f67a142b55bc12ebe13b989cd40f1c1fb0a78
Alexandre PLU <alexandre.p42@proton.me>
2024-08-18 18:45:21 +0200

    Définition du nombre de planètes et affichages

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello universe!");
-        
+        int nbPlanetes = 8;
+
+        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de "+nbPlanetes);
+
+        nbPlanetes = 9;
+
+        System.out.println("Il y'a quelques années cependant, elles étaient au nombre de "+nbPlanetes);
     }
 }
