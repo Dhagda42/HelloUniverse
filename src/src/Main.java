4f9b59efd3df85ef499292c1f43c566b6c248e0d
Alexandre PLU <alexandre.p42@proton.me>
2024-08-22 13:41:25 +0200

    Modification avec premières variables String

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello universe!");
+        String phrase = ("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ");
         int nbPlanetes = 8;
-
-        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de "+nbPlanetes);
-
+        System.out.println(phrase + nbPlanetes);
+        phrase = ("Il y a quelques années cependant, elles étaient au nombre de : ");
         nbPlanetes = 9;
-
-        System.out.println("Il y'a quelques années cependant, elles étaient au nombre de "+nbPlanetes);
+        System.out.println(phrase + nbPlanetes);
     }
 }
