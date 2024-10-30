8731c30491ab918185143c6097dff6f9023366b2
Alexandre PLU <alexandre.p42@proton.me>
2024-10-30 15:54:16 +0100

    Amélioration du code avec blocs imbriqués et else if

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,14 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        int annee = 2006;
+        int annee = 2024;
         int nbPlanetes = 8;
 
-        if (annee < 2006){
-            nbPlanetes = 9;
+        if (annee < 1600 || annee > 2024){
+            System.out.println("Le programme ne peut pas fournir de résultat pour l'année" +annee);
         }
-        else{
-            nbPlanetes = 8;
+        else {
+            if (annee < 1781){
+                nbPlanetes = 7;
+            }
+            else if (annee < 1846){
+                nbPlanetes = 8;
+            }
+            else if (annee < 2006){
+                nbPlanetes = 9;
+            }
+            else{
+                nbPlanetes = 8;
+            }
         }
-        System.out.printf("En %d, le nombre de planète est de %d", annee, nbPlanetes);
+        System.out.printf("Le nombre de planètes dans le système solaire en %d est de %d", annee, nbPlanetes);
+
     }
 }
